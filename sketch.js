d3d0411245f93a7136af77d0b38c77c51388504d
chunjunghee <dkddkd0311@naver.com>
2014-11-19 18:15:23 +0900

    dd

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var bg;
+
+var img;  
+var img2;
+var img3;
+
+
+function setup()
+{
+    createCanvas(641,901);
+    bg = loadImage("bg.png");
+    img = loadImage("sugarBox.png");
+    img2 = loadImage("midBox.png");
+
+}
+
+
+function draw()
+{
+    background(bg);
+
+    fill(237,112,50);
+    noStroke();
+ 
+    image(img,254,0);
+
+    } 
