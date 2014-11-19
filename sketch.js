051051cb96079e30325b02c7660ac041cf2537c6
chunjunghee <dkddkd0311@naver.com>
2014-11-19 20:03:02 +0900

    ff

--- sketch.js
+++ sketch.js
@@ -70,14 +70,14 @@ function draw()
     }  
 
     image(photo8,582,count1+100);
-    count1=count1+0.5;
+    count1=count1+1;
      if(count1>360){
     count1=0;
     }  
 
 
     image(photo9,590,count2+50);
-    count2=count2+1;
+    count2=count2+1.5;
      if(count2>360){
     count2=0;
     }  
