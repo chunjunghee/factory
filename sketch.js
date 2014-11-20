d8e794c0e81a13402796b5f3011facea4256343a
chunjunghee <dkddkd0311@naver.com>
2014-11-20 14:16:16 +0900

    ff

--- sketch.js
+++ sketch.js
@@ -169,8 +169,8 @@ function draw()
             f=false;
         }else{
             fill(255,255,0);
-            ellipse(312,449+c*5,100,100);
-            c= c+1;
+            ellipse(312,449+c*5,30,30);
+            c= -c+1;
         }
 
     }
